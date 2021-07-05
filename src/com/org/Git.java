de246fe7b4ad4e1ea2b49c3fbba384045d32b564
YamunaRavooru <malini.mahathi@gmail.com>
2021-07-05 10:03:58 +0530

    first push

--- src/com/org/Git.java
+++ src/com/org/Git.java
@@ -3,5 +3,9 @@ package com.org;
 public class Git {
 public static void main(String[] args) {
 	System.out.println("welcome to java");
+	System.out.println("123");
+	System.out.println("123");
+	System.out.println("123");
+	System.out.println("123");
 }
 }
